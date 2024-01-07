0b1d5ba0e537fef821974c0362cf474cb24cc192
shadowbubble1 <kaushalkishoresharma07@gmail.com>
2024-01-07 13:11:09 +0530

    These are initial files of integrate different logins.

---
+++ Integrate_Different_logins/app/src/main/java/com/androidapp/integrate_different_logins/MainActivity.java
@@ -0,0 +1,15 @@
+package com.androidapp.integrate_different_logins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
